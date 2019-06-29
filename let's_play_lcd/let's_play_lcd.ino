91cfa27aac819ba5be1dfe7d7c5898f8beb626e1
Kahloyx <destroyman54930@gmail.com>
2019-06-29 13:50:57 +0200

    First commit

---
+++ let's_play_lcd/let's_play_lcd.ino
@@ -0,0 +1,37 @@
+#include <LiquidCrystal.h>
+int RS = 7;
+int E = 5;
+int D4 = 3;
+int D5 = 2;
+int D6 = 1;
+int D7 = 0;
+LiquidCrystal lcd (RS, E, D4, D5, D6, D7); //(rs,e,d4,d5,d6,d7)
+String tst = "";
+int timer = 200;
+int j = 0;
+void setup()
+{
+  lcd.begin(16, 2);
+  for (int i = 0; i < 17; i++)
+  {
+    lcd.clear();
+    lcd.print(tst);
+    tst = tst + ".";
+    delay(100);
+    lcd.setCursor(0, 1);
+  }
+}
+void loop()
+{
+  while( j < 11, j++)
+  {
+    delay(timer);
+    lcd.clear();
+    lcd.print("|_(");
+    lcd.setCursor(3,0);
+    lcd.print((char)175);
+    lcd.setCursor(4,0);
+    lcd.print(")_|");
+  }
+  j = 42;
+}
